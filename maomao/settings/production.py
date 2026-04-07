ee0af998f6cf44c1d423eaa7731dcb760bb65900
Dani <dani@Fabian.local>
2026-04-06 21:56:37 -0400

    Desactivar SMTP en produccion para evitar worker timeout

--- maomao/settings/production.py
+++ maomao/settings/production.py
@@ -14,12 +14,7 @@ DATABASES = {
 }
 
 # Email con Gmail
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
 
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
